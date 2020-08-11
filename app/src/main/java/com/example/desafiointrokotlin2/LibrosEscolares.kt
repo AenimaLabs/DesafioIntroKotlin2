5d485a1c5e1510ba1685336a6189f78ed930142e
Damian Aenima <aenimalabs@gmail.com>
2020-08-11 16:39:56 -0400

    final posible

--- app/src/main/java/com/example/desafiointrokotlin2/LibrosEscolares.kt
+++ app/src/main/java/com/example/desafiointrokotlin2/LibrosEscolares.kt
@@ -1,11 +1,23 @@
 package com.example.desafiointrokotlin2
 
-data class LibrosEscolares (var nombrelibros: String,
-                            var isbn: String,
-                            var añopublicacion: Int,
-                            var editorial: String,
-                            var cantidadpáginas: Int,
-                            var precio: Int,
-                            var autor: String,
-                            var librodigital: Boolean = true) {
+import android.util.Log
+
+data class LibrosEscolares (val nombrelibros: String,
+                            val isbn: Long,
+                            val añopublicacion: Short,
+                            val editorial: String,
+                            val cantidadpáginas: Int,
+                            val precio: Double,
+                            val autor: String,
+                            val tipodelibro: Char) {
+
+
+    fun precioFormateado(): String {
+        return "\$${this.precio}"
+    }
+    fun imprimir(): String {
+        return "$nombrelibros - $isbn - $añopublicacion - $editorial - $cantidadpáginas -${precioFormateado()} - $autor - $tipodelibro"
+    }
 }
+}
+
--- app/src/main/java/com/example/desafiointrokotlin2/MainActivity.kt
+++ app/src/main/java/com/example/desafiointrokotlin2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.desafiointrokotlin2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var cantidadlibros: Int = 4
+
+        var librosEscolares1 = LibrosEscolares ("abacada", 12414, 1950, "cccc", 500, 20.5, "nadie", 'D')
+
+
+        Log.wtf("algo", librosEscolares1.precioFormateado())
+
+
+
     }
+
+
 }
